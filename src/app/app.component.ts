b5f9e76feb3cf3e2d525d0b93bd5e33ca0129c9f
Andry Mendoza <andryjavier05@gmail.com>
2025-02-12 21:37:37 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
 import { ThemeServiceService } from './services/theme-service.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
