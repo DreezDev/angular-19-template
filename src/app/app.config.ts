080719a4f8adfb6a5a9f5bc9b42ceb50c5fffa74
Andry Mendoza <andryjavier05@gmail.com>
2025-04-17 14:24:56 -0400

    refactor: replace DashboardComponent with HomeComponent and remove unused auth guard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import {
-  provideHttpClient,
-  withFetch
-} from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
@@ -19,10 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from '@pages/dashboard/dashboard.component';
+import { HomeComponent } from '@pages/home/home.component';
 import { NotFoundComponent } from '@pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
-    pathMatch: 'full',
+    component: HomeComponent,
   },
   {
     path: '**',
